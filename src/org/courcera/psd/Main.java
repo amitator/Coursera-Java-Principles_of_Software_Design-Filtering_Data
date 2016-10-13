d625eeeb225a12b84ee6842553cd053ead1e7130
Igor Prus <prus.igor@gmail.com>
2016-10-14 00:04:29 +0300

    added Main class for psvm

---
+++ src/org/courcera/psd/Main.java
@@ -0,0 +1,13 @@
+package src.org.courcera.psd;
+
+/**
+ * This is Main, just to start.
+ *
+ * @author (Igor Prus)
+ * @version (0.1)
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
